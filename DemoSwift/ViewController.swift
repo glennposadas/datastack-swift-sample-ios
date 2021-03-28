52f84e640bc0797a4d8e2598f1c2b02bfc6b4ba0
Glenn Posadas <hello@glennvon.com>
2021-03-28 13:20:30 +0800

    implement diffable datasource

--- DemoSwift/ViewController.swift
+++ DemoSwift/ViewController.swift
@@ -4,66 +4,99 @@ import CoreData
 class ViewController: UITableViewController, NSFetchedResultsControllerDelegate {
   
   var dataStack: DATAStack
-  var items = [NSFetchRequestResult]()
+  var items = [User]()
   
-  var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>!
+  var fetchedResultsController: NSFetchedResultsController<User>!
   
-  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChangeContentWith snapshot: NSDiffableDataSourceSnapshotReference) {
-    items = fetchedResultsController.fetchedObjects!
-    tableView.reloadSections(IndexSet.init(integer: 0), with: .automatic)
+  enum TableSection {
+    case main
   }
-
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-    
-    
-    let item = self.items[indexPath.row] as! User
-    if let name = item.value(forKey: "name") as? String, let createdDate = item.value(forKey: "createdDate") as? NSDate {
-      cell!.textLabel?.text = name + " - " + createdDate.description
-    }
-    
-    return cell!
+  var dataSource: UITableViewDiffableDataSource<TableSection, User>!
+  
+  func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    configureSnapshot()
   }
   
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return items.count
+  func configureSnapshot() {
+      
+      var snapshot = NSDiffableDataSourceSnapshot<TableSection, User>()
+      
+      do {
+          try fetchedResultsController.performFetch()
+          
+          snapshot.appendSections([.main])
+          
+          self.items = fetchedResultsController.fetchedObjects ?? []
+          
+          snapshot.appendItems(self.items)
+          
+          dataSource.apply(snapshot, animatingDifferences: true)
+          
+      } catch let error {
+          print(error.localizedDescription)
+      }
   }
+  
+//  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+//
+//
+//    let item = self.items[indexPath.row] as! User
+//    if let name = item.value(forKey: "name") as? String, let createdDate = item.value(forKey: "createdDate") as? NSDate {
+//      cell!.textLabel?.text = name + " - " + createdDate.description
+//    }
+//
+//    return cell!
+//  }
+//
+//  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//    return items.count
+//  }
   
   init(dataStack: DATAStack) {
     self.dataStack = dataStack
     
     super.init(style: .plain)
-  
+    
+    let request: NSFetchRequest = NSFetchRequest<User>(entityName: "User")
+    request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+    
+    fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: dataStack.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+    fetchedResultsController.delegate = self
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
+  func configureDataSource() {
+    dataSource = .init(tableView: tableView, cellProvider: { (tableView, indexPath, company) -> UITableViewCell? in
+      
+      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
+      
+      let item = self.items[indexPath.row] as! User
+      if let name = item.value(forKey: "name") as? String, let createdDate = item.value(forKey: "createdDate") as? NSDate {
+        cell.textLabel?.text = name + " - " + createdDate.description
+      }
+      
+      return cell
+    })
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-    self.tableView.dataSource = self
+    configureDataSource()
+    configureSnapshot()
+    //self.tableView.dataSource = self
     
     let backgroundButton = UIBarButtonItem(title: "Background", style: .done, target: self, action: #selector(self.createBackground))
     self.navigationItem.rightBarButtonItem = backgroundButton
     
     let mainButton = UIBarButtonItem(title: "Main", style: .done, target: self, action: #selector(ViewController.createMain))
     self.navigationItem.leftBarButtonItem = mainButton
-    
-    let request: NSFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
-    request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
-    
-    fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: dataStack.viewContext, sectionNameKeyPath: nil, cacheName: nil)
-    fetchedResultsController.delegate = self
-    try! fetchedResultsController.performFetch()
-    
-    items = fetchedResultsController.fetchedObjects!
-    
   }
-  
-  
   
   @objc func createBackground() {
     self.dataStack.performInNewBackgroundContext { backgroundContext in
